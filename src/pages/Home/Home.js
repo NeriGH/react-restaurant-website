3d9482ed392c79f0140d6b8902f2f144eae325b9
NeriGH <alexis.neri@outlook.fr>
2023-03-09 15:50:37 +0100

    Bootstrap implementation

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,13 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./Home.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <p class="h1">
+          Edit <code>src/Home.js</code> and save to reload.
         </p>
         <a
           className="App-link"
