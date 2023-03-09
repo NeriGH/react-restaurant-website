fae441ddd1dc5211329929db21c2739ebec86250
NeriGH <alexis.neri@outlook.fr>
2023-03-09 16:47:14 +0100

    Uninstall Bootstrap test

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,23 +1,14 @@
 import logo from "./logo.svg";
 import "./Home.css";
+import "../../components/Header/Header";
+import Header from "../../components/Header/Header";
+import Slider from "../../components/Slider/Slider";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p class="h1">
-          Edit <code>src/Home.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Slider />
     </div>
   );
 }
